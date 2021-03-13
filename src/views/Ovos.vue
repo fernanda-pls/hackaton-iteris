ab834d5f071dcf8c2290f05b4ecac4da9e861a28
PaulaCBS <paulasalvado2019@gmail.com>
2021-03-13 16:30:49 -0300

    muda pagina ovos de pascoa

--- src/views/Ovos.vue
+++ src/views/Ovos.vue
@@ -2,51 +2,48 @@
   <v-container class="container">
     <h2 class="text-h5 text-center mb-3 mt-6">Ovos de Páscoa</h2>
 
-    <p>Escolha os produtores de ovos de Páscoa caseiros de acordo com a sua cidade:</p>
+    <p class="text-center">Escolha os produtores de ovos de Páscoa caseiros de acordo com a sua cidade:</p>
 
-    <v-row align="center" justify="space-around" class="mt-5">
-      <v-btn @click="mudaFiltro" class="ma-2" primary>AC - Rio Branco</v-btn>
-      <v-btn @click="mudaFiltro" class="ma-2" primary>AM - Manaus</v-btn>
-      <v-btn @click="mudaFiltro" class="ma-2" primary>AP - Macapá</v-btn>
+    <v-row>
+       <v-card
+        v-for="ovo in OvosPascoa" 
+        :key="ovo.nome"
+        class="mx-auto my-12"
+        max-width="374"
+        dark
+      >
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          :src="ovo.imagem"
+        ></v-img>
+
+        <v-card-title class="pt-5 pb-2">{{ ovo.nome }}</v-card-title>
+
+        <v-card-text class="my-0 px-4">
+          <div><span class="font-weight-bold">Endereço:</span> {{ ovo.local.endereco }}</div>
+          <div><span class="font-weight-bold">Local:</span> {{ ovo.local.nome }}</div>
+          <div><span class="font-weight-bold">Sabor:</span> {{ ovo.sabor }}</div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-actions>
+          <v-card-text class="text-right preço text--white">
+            R$ {{ ovo.preco }}
+          </v-card-text>
+        </v-card-actions>
+      </v-card>
     </v-row>
 
-    {{ }}
-    <v-card
-      v-for="ovo in OvosPascoa" :key="ovo.nome"
-      :loading="loading"
-      class="mx-auto my-12"
-      max-width="374"
-      dark
-    >
-      <template slot="progress">
-        <v-progress-linear
-          color="deep-purple"
-          height="10"
-          indeterminate
-        ></v-progress-linear>
-      </template>
-
-      <v-img
-        height="250"
-        :src="ovo.imagem"
-      ></v-img>
-
-      <v-card-title class="pt-5 pb-2">{{ ovo.nome }}</v-card-title>
-
-      <v-card-text class="my-0 px-4">
-        <div><span class="font-weight-bold">Endereço:</span> {{ ovo.local.endereco }}</div>
-        <div><span class="font-weight-bold">Local:</span> {{ ovo.local.nome }}</div>
-        <div><span class="font-weight-bold">Sabor:</span> {{ ovo.sabor }}</div>
-      </v-card-text>
-
-      <v-divider class="mx-4"></v-divider>
-
-      <v-card-actions>
-        <v-card-text class="text-right preço text--white">
-          R$ {{ ovo.preco }}
-        </v-card-text>
-      </v-card-actions>
-    </v-card>
+    
 
   </v-container>
 </template>
@@ -54,27 +51,9 @@
 <script>
 export default {
   name: "Ovos",
-  filtro: "",
-  Ovosfiltrados: [],
   data() {
     return {
       OvosPascoa: []
-    }
-  },
-  methods: {
-    mudaFiltro(e) {
-      let botao = e.target.textContent
-      if(botao == "AC - Rio Branco"){
-        this.filtro = "69915894"
-      } else if(botao == "AM - Manaus"){
-        this.filtro = "69098033"
-      } else if(botao == "AP - Macapá"){
-        this.filtro = "68906477"
-      }
-
-      this.Ovosfiltrados = this.OvosPascoa.filter( ovos => {
-        return ovos.local.cep == this.filtro
-      })
     }
   },
   created() {
