5d01a18ed965a3896a3a4cb37764c7af996be0bc
PaulaCBS <paulasalvado2019@gmail.com>
2021-03-13 11:24:16 -0300

    atualiza home card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,29 +9,18 @@
           <div class="overline mb-4">
             Coleta Seletiva
           </div>
-          <v-list-item-title class="headline mb-1">
-            Procure postos de coleta próximos
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Ajude a reduzir os impactos ambientais relacionados ao descarte incorreto do lixo
-          </v-list-item-subtitle>
+          <v-list-item-action-text>
+            Procure postos de coleta e ajude a reduzir os impactos ambientais relacionados ao descarte incorreto do lixo
+          </v-list-item-action-text>
         </v-list-item-content>
-
-        <v-list-item-avatar
-          tile
-          size="80"
-          color="grey"
-        ></v-list-item-avatar>
       </v-list-item>
 
       <v-card-actions>
         <v-btn
-          outlined
+          elevation="2"
           rounded
-          text
-        >
-         Buscar
-        </v-btn>
+          small
+        > Buscar </v-btn>
       </v-card-actions>
     </v-card>
   </v-container>
