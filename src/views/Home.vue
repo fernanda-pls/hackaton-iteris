8313b589add3b81f4d17d7d6883680c474c78df1
PaulaCBS <paulasalvado2019@gmail.com>
2021-03-13 11:21:13 -0300

    add home cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
   <v-container>
-    Home
+    <v-card
+      elevation="2"
+      shaped
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">
+            Coleta Seletiva
+          </div>
+          <v-list-item-title class="headline mb-1">
+            Procure postos de coleta próximos
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Ajude a reduzir os impactos ambientais relacionados ao descarte incorreto do lixo
+          </v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-avatar
+          tile
+          size="80"
+          color="grey"
+        ></v-list-item-avatar>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn
+          outlined
+          rounded
+          text
+        >
+         Buscar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: Home
+  name: 'Home'
 }
 </script>
 
