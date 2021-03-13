05a6fa7b0a6c8a4f234ca65aaa02c33ebe89c368
PaulaCBS <paulasalvado2019@gmail.com>
2021-03-13 10:23:36 -0300

    add rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import PostosColeta from '../views/PostosColeta.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/PostosColeta',
+    name: 'Coleta',
+    component: PostosColeta
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/PostosColeta.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    Postos de Coleta
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: PostosColeta
+}
+</script>
+
+<style scoped>
+
+</style>
