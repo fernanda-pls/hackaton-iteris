a1cca7123f1d5393095f2dbf406d8026cce94095
PaulaCBS <paulasalvado2019@gmail.com>
2021-03-13 10:29:49 -0300

    atualiza nome rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import PostosColeta from '../views/PostosColeta.vue'
+import PostosColeta from '../views/ListaColeta.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +12,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/PostosColeta',
+    path: '/ListaColeta',
     name: 'Coleta',
-    component: PostosColeta
+    component: ListaColeta
   }
 ]
 
