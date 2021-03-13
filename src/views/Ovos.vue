6c7f27e970a945ac73608c05a4e5737a482d017c
Fernanda Pls <ferrnanda.pls@gmail.com>
2021-03-13 14:30:46 -0300

    adiciona cidades em Ovos

--- src/views/Ovos.vue
+++ src/views/Ovos.vue
@@ -1,6 +1,20 @@
 <template>
-  <v-container>
+  <v-container class="container">
     <h2 class="text-h5 text-center mb-3 mt-6">Ovos de Páscoa</h2>
+
+    <p>Escolha os produtores de ovos de Páscoa caseiros por cidade:</p>
+
+    <v-row align="center" justify="space-around" class="mt-5">
+      <router-link to="/ListaColeta">
+        <v-btn class="ma-2" primary>AC - Rio Branco</v-btn>
+      </router-link>
+      <router-link to="/ListaColeta">
+        <v-btn class="ma-2" primary>AM - Manaus</v-btn>
+      </router-link>
+      <router-link to="/ListaColeta">
+        <v-btn class="ma-2" primary>AP - Macapá</v-btn>
+      </router-link>
+    </v-row>
   </v-container>
 </template>
 
@@ -11,5 +25,8 @@ export default {
 </script>
 
 <style scoped>
+.container a {
+  text-decoration: none;
+}
 </style>
 
