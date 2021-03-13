dd839f9f19d2cc3a2b51389090b93b8b874877a1
PaulaCBS <paulasalvado2019@gmail.com>
2021-03-13 12:19:55 -0300

    muda tamanho do cta da home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="home-container">
+  <v-container class="home-container d-flex flex-column align-center">
     <h2 class="text-h2 text-center mb-3 mt-6">Conecta</h2>
     <v-img
       class="inicio-imagem mb-4"
@@ -8,17 +8,16 @@
     ></v-img>
     <p class="text">A Conecta é uma plataforma que aproxima os cidadãos dos postos de coleta espalhados pelo país. Nossa missão é tornar a reciclagem uma realidade, em prol do desenvolvimento sustentável.</p>
 
-    <h3 class="text-h4 text-left mb-3 mt-6">Coleta Seletiva</h3>
+    <h3 class="text-h4 text-left mb-3 mt-6 align-self-start">Coleta Seletiva</h3>
     <p class="text">A Coleta Seletiva é um mecanismo de recolha dos resíduos, os quais são classificados de acordo com sua origem e depositados em contentores indicados por cores.</p>
     <p class="text">Ou seja, eles podem ser resíduos orgânicos ou materiais recicláveis como papel, pástico, vidro, dentre outros. Além deles, materiais hospitalares e radioativos têm um destino diferente.</p>
 
     <router-link to="/ListaColeta">
       <v-btn
         outlined
-        block
         color="#26A69A"
         elevation="1"
-        class="my-1 coleta-btn"
+        class="my-2 my-sm-5 coleta-btn"
       >Buscar Postos de Coleta</v-btn>
     </router-link>
   </v-container>
