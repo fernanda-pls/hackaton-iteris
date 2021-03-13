80d2987b2d4ec67721d2c532cfefc6fe84f389e9
Fernanda Pls <ferrnanda.pls@gmail.com>
2021-03-13 14:35:53 -0300

    altera o Ovos.vue

--- src/views/Ovos.vue
+++ src/views/Ovos.vue
@@ -2,18 +2,12 @@
   <v-container class="container">
     <h2 class="text-h5 text-center mb-3 mt-6">Ovos de Páscoa</h2>
 
-    <p>Escolha os produtores de ovos de Páscoa caseiros por cidade:</p>
+    <p>Escolha os produtores de ovos de Páscoa caseiros de acordo com a sua cidade:</p>
 
     <v-row align="center" justify="space-around" class="mt-5">
-      <router-link to="/ListaColeta">
-        <v-btn class="ma-2" primary>AC - Rio Branco</v-btn>
-      </router-link>
-      <router-link to="/ListaColeta">
-        <v-btn class="ma-2" primary>AM - Manaus</v-btn>
-      </router-link>
-      <router-link to="/ListaColeta">
-        <v-btn class="ma-2" primary>AP - Macapá</v-btn>
-      </router-link>
+      <v-btn class="ma-2" primary>AC - Rio Branco</v-btn>
+      <v-btn class="ma-2" primary>AM - Manaus</v-btn>
+      <v-btn class="ma-2" primary>AP - Macapá</v-btn>
     </v-row>
   </v-container>
 </template>
