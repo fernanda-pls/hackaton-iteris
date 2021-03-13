e481f623237b8ebbe00ca97697a6dadf5344276a
Fernanda Pls <ferrnanda.pls@gmail.com>
2021-03-13 11:08:59 -0300

    Adiciona Inicio e AppTopBar

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Inicio from '../views/Inicio.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Inicio',
+    component: Inicio
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Inicio.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <h2 class="text-h2 text-center mb-3 mt-6">Conecta</h2>
+    <v-img
+      class="inicio-imagem mb-4"
+      src="https://www.ferrovelhocoradin.com.br/wp-content/uploads/2019/06/Lixeiras.png"
+      alt="Imagem Reciclagem"
+    ></v-img>
+    <p>A Conecta é uma plataforma que aproxima os cidadãos dos postos de coleta espalhados pelo país. Nossa missão é tornar a reciclagem uma realidade, em prol do desenvolvimento sustentável.</p>
+
+    <h3 class="text-h4 text-left mb-3 mt-6">Coleta Seletiva</h3>
+    <p>A Coleta Seletiva é um mecanismo de recolha dos resíduos, os quais são classificados de acordo com sua origem e depositados em contentores indicados por cores.</p>
+    <p>Ou seja, eles podem ser resíduos orgânicos ou materiais recicláveis como papel, plástico, vidro, dentre outros. Além deles, materiais hospitalares e radioativos têm um destino diferente.</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Inicio"
+};
+</script>
+
+<style scoped>
+.inicio-imagem {
+  border-radius: 5px;
+}
+</style>
